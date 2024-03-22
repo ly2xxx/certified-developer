9ec0d1b2e16f47e2826e19a4d4ef4b0f52972173
ly2xxx <ly2xxx@hotmail.com>
2024-03-22 22:08:08 +0000

    lab6 code doesn't work out of the box

--- MLOps_Professional/lab6/sample/Falcon_HF_Pipelines.py
+++ MLOps_Professional/lab6/sample/Falcon_HF_Pipelines.py
@@ -1,4 +1,6 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
+# from transformers import AutoTokenizer
+#, AutoModelForCausalLM
+from transformers import AutoModel
 import transformers
 import torch
 import argparse
@@ -6,13 +8,21 @@ import time
 
 
 def main(FLAGS):
-    
+    #Constructs the model identifier based on the Falcon version specified in the command-line arguments.
     model = f"tiiuae/falcon-{FLAGS.falcon_version}"
     
+    #Initializes a tokenizer for the specified model, allowing text inputs to be processed for the model.
+    # tokenizer = AutoTokenizer.from_pretrained(model, trust_remote_code=True)
+    tokenizer = AutoModel.from_pretrained(model, trust_remote_code=True)
     
-    tokenizer = AutoTokenizer.from_pretrained(model, trust_remote_code=True)
-    
-        
+    #Creates a text generation pipeline using the transformers library. This pipeline abstracts model loading, tokenization, and inference
+#     Arguments provided to the pipeline creation:
+# "text-generation": Specifies the pipeline type.
+# model=model: Specifies the pre-trained model to be used.
+# tokenizer=tokenizer: Specifies the tokenizer to be used.
+# torch_dtype=torch.bfloat16: Sets the data type for torch tensors to bfloat16.
+# trust_remote_code=True: Allows remote code execution for the tokenizer.
+# device_map="auto": Automatically selects the device for inference.    
     generator = transformers.pipeline(
         "text-generation",
         model=model,
@@ -22,14 +32,23 @@ def main(FLAGS):
         device_map="auto",
     )
 
+#     User Interaction Loop:
+# user_input = "start": Initializes the user input to begin the loop.
+# while user_input != "stop":: Starts a loop that continues until the user enters "stop".
     user_input = "start"
 
     while user_input != "stop":
-
+        #Prompts the user to input a text string for the model to generate text based on
         user_input = input(f"Provide Input to {model} parameter Falcon (not tuned): ")
         
         start = time.time()
-
+        #Generates text sequences using the pre-trained model and provided input.
+        # f""" {user_input}""": Formats the user input for model processing.
+        # max_length=FLAGS.max_length: Specifies the maximum length of the generated text.
+        # do_sample=False: Disables sampling during text generation.
+        # top_k=FLAGS.top_k: Sets the number of highest probability tokens to consider.-
+        # num_return_sequences=1: Specifies the number of generated sequences to return.
+        # eos_token_id=tokenizer.eos_token_id: Specifies the end-of-sequence token ID.
         if user_input != "stop":
             sequences = generator( 
             f""" {user_input}""",
